3b72bfe0c7c99361ed5a4436d7873b3932040b26
豆粒 <zbm49717@gmail.com>
2026-07-16 20:48:33 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "neko-geme-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./img/gemes192.png",
+  "./img/gemes512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
